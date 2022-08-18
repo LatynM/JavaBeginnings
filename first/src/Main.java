54e4b06799ce866e726fc1b1295419dd2317ae7a
LatynM <111091315+LatynM@users.noreply.github.com>
2022-08-18 10:28:45 -0300

    Add files via upload

---
+++ first/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String si = "Hola mundo";
+        String nome = scan.nextLine();
+        System.out.println(si);
+        System.out.println("Buenas"+nome);
+
+
+    }
+}
