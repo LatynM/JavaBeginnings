7972a73f76fc6dd4b94e4bfd67b23cf895e88cf5
LatynM <111091315+LatynM@users.noreply.github.com>
2022-08-24 14:57:51 -0300

    si

---
+++ Variables1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+    String nombre="Marcelo";
+    int edad=19;
+    double salario=27000.50;
+    boolean carnet=true;
+
+    }
+}
