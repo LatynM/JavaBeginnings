471b9ec989e88be2f283baa1c7b27fd72577c476
LatynM <111091315+LatynM@users.noreply.github.com>
2022-08-24 15:30:59 -0300

    Mathé

---
+++ JavaMath3/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+    int opcion;
+
+	double seno = Math.sin(0.6);
+    double coseno = Math.cos(0.7);
+    double tangente = Math.tan(0.8);
+    double arcotngnt = Math.atan(0.8);
+    double atan2 = Math.atan2(2.0,3.0);
+    double exp = Math.exp(1);
+    double logaritmo = Math.log(16.0);
+    double pi = Math.PI;
+    double E = Math.E;
+
+    }
+}
