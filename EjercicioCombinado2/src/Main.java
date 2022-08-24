4549e714b10cad3cad471b621faa8167364907a8
LatynM <111091315+LatynM@users.noreply.github.com>
2022-08-24 15:10:41 -0300

    ejercicio 2

---
+++ EjercicioCombinado2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        double resultado;
+
+        resultado=(6.0/5.0-2.0/3.0*7.0/2.0+2.0/30.0)/((1.0/3.0)/5.0);
+
+        System.out.println(resultado);
+    }
+}
